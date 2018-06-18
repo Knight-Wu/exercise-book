74d19bc325a5845cfd51c46870f3f3914bcbb764
Knight-WT <1239562508guge@gmail.com>
2018-06-18 08:37:00 +0800

    整理exerciseBook

--- leedcode/src/main/java/algorithm/array/SearchIn2DMatrix.java
+++
@@ -1,8 +0,0 @@
-package algorithm.array;
-
-public class SearchIn2DMatrix {
-        public boolean searchMatrix(int[][] matrix, int target) {
-            int m = matrix[0].length;
-            return false;
-        }
-}
